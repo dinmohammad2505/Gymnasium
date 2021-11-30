309b7d22f83317685bcf4b77d2c0bad5552e9595
dinmohammad2505 <dinmohammad2505@gmail.com>
2021-11-30 22:17:20 +0600

    first half done

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  'use strict';
+  // banner slider
+  $('.banner-slider').slick({
+  arrows: false,
+  slidesToShow: true,
+  dots: true,
+  // autoplay: true,
+  sautoplaySpeed:500,
+});
+// About video
+  $('.venobox').venobox(); 
+
+});
+	
